0d85ffb3e7880b86f0800736fe9080c6f9266e71
hezzie <hezronchelimo.hc@gmail.com>
2020-10-05 23:29:54 +0200

    added firebase support

--- src/routes/api/authRoute.js
+++ src/routes/api/authRoute.js
@@ -1,14 +1,26 @@
 import express from "express";
+var jwt = require("jsonwebtoken");
+import { writeFbData } from "../../utils/fb";
+
+const pubKey = `-----BEGIN CERTIFICATE-----\nMIIDBTCCAe2gAwIBAgIQQiR8gZNKuYpH6cP+KIE5ijANBgkqhkiG9w0BAQsFADAtMSswKQYDVQQDEyJhY2NvdW50cy5hY2Nlc3Njb250cm9sLndpbmRvd3MubmV0MB4XDTIwMDgyODAwMDAwMFoXDTI1MDgyODAwMDAwMFowLTErMCkGA1UEAxMiYWNjb3VudHMuYWNjZXNzY29udHJvbC53aW5kb3dzLm5ldDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMkymupuRhTpZc+6CBxQpL0SaAb+8CzLiiDyx2xRoecjojvKN2pKKjIX9cejMSDRoWaOnZCK4VZVX1iYRCWT1WkHb8r1ZpSGa7oXG89zxjKjwG46tiamwdZjJ7Mhh8fqLz9ApucY/LICPMJuu6d56LKs6hb4OpjylTvsNUAa+bHg1NgMFNg0fPCxdr9N2Y4J+Jhrz3VDl4oU0KDZX/pyRXblzA8kYGWm50dh5WB4WoB8MtW3lltVrRGj8/IgTf9GxpBsO9OWgwVByZHU7ctZs7AmUbq/59Ipql7vSM6EsoquXdMiq0QOcZAPitwzHkTKrmeULz0/RHnuBGXxS/e8wX0CAwEAAaMhMB8wHQYDVR0OBBYEFGcWXwaqmO25Blh2kHHAFrM/AS2CMA0GCSqGSIb3DQEBCwUAA4IBAQDFnKQ98CBnvVd4OhZP0KpaKbyDv93PGukE1ifWilFlWhvDde2mMv/ysBCWAR8AGSb1pAW/ZaJlMvqSN/+dXihcHzLEfKbCPw4/Mf2ikq4gqigt5t6hcTOSxL8wpe8OKkbNCMcU0cGpX5NJoqhJBt9SjoD3VPq7qRmDHX4h4nniKUMI7awI94iGtX/vlHnAMU4+8y6sfRQDGiCIWPSyypIWfEA6/O+SsEQ7vZ/b4mXlghUmxL+o2emsCI1e9PORvm5yc9Y/htN3Ju0x6ElHnih7MJT6/YUMISuyob9/mbw8Vf49M7H2t3AE5QIYcjqTwWJcwMlq5i9XfW2QLGH7K5i8\n-----END CERTIFICATE-----`;
 
 const authRouter = express.Router();
 export default authRouter;
-authRouter.post("auth", () => {
-  console.log("############################");
+
+authRouter.post("/auth", (req, res, next) => {
+  // AigFvA2ubOR67BZFOwRi._ZyFY7Q9qD~8~
+  jwt.verify(req.headers.authorization, pubKey, { algorithms: ["RS256"] }, function (err, decoded) {
+    console.log("############################", err, decoded);
+    writeFbData("users", decoded);
+  });
+  res.status(200).send({
+    message: "Welcome to MS test API.",
+  });
 });
 /**
  * @swagger
  *
- * /auth/signin:
+ * /auth:
  *    post:
  *      summary: User login
  *      tags: [Users]
---
+++ src/utils/fb.js
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import * as firebase from "firebase";
+
+const {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+} = process.env;
+
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+};
+
+export default firebase.initializeApp(firebaseConfig);
+export const db = firebase.database();
+
+export const writeFbData = async (url, data) => {
+  return await db.ref(url).push(data);
+};
+export const editFbData = (url, data) => {
+  db.ref(url).set(data);
+};
+export const readFbData = (url, cb) =>
+  db.ref(url).on("value", (snapshot) => {
+    cb(snapshot.val() || {});
+  });
