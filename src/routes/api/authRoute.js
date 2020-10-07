cadfb0abb04a15e7f91d912196d5cc628be957c2
hezzie <hezronchelimo.hc@gmail.com>
2020-10-07 08:52:48 +0200

    auth route: - validated the ad token - handled local signin hide important keys

--- src/routes/api/authRoute.js
+++ src/routes/api/authRoute.js
@@ -1,21 +1,52 @@
+require("dotenv").config();
 import express from "express";
-var jwt = require("jsonwebtoken");
-import { writeFbData } from "../../utils/fb";
+import jwt from "jsonwebtoken";
+import { writeFbData, readFbData } from "../../utils/fb";
 
-const pubKey = `-----BEGIN CERTIFICATE-----\nMIIDBTCCAe2gAwIBAgIQQiR8gZNKuYpH6cP+KIE5ijANBgkqhkiG9w0BAQsFADAtMSswKQYDVQQDEyJhY2NvdW50cy5hY2Nlc3Njb250cm9sLndpbmRvd3MubmV0MB4XDTIwMDgyODAwMDAwMFoXDTI1MDgyODAwMDAwMFowLTErMCkGA1UEAxMiYWNjb3VudHMuYWNjZXNzY29udHJvbC53aW5kb3dzLm5ldDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMkymupuRhTpZc+6CBxQpL0SaAb+8CzLiiDyx2xRoecjojvKN2pKKjIX9cejMSDRoWaOnZCK4VZVX1iYRCWT1WkHb8r1ZpSGa7oXG89zxjKjwG46tiamwdZjJ7Mhh8fqLz9ApucY/LICPMJuu6d56LKs6hb4OpjylTvsNUAa+bHg1NgMFNg0fPCxdr9N2Y4J+Jhrz3VDl4oU0KDZX/pyRXblzA8kYGWm50dh5WB4WoB8MtW3lltVrRGj8/IgTf9GxpBsO9OWgwVByZHU7ctZs7AmUbq/59Ipql7vSM6EsoquXdMiq0QOcZAPitwzHkTKrmeULz0/RHnuBGXxS/e8wX0CAwEAAaMhMB8wHQYDVR0OBBYEFGcWXwaqmO25Blh2kHHAFrM/AS2CMA0GCSqGSIb3DQEBCwUAA4IBAQDFnKQ98CBnvVd4OhZP0KpaKbyDv93PGukE1ifWilFlWhvDde2mMv/ysBCWAR8AGSb1pAW/ZaJlMvqSN/+dXihcHzLEfKbCPw4/Mf2ikq4gqigt5t6hcTOSxL8wpe8OKkbNCMcU0cGpX5NJoqhJBt9SjoD3VPq7qRmDHX4h4nniKUMI7awI94iGtX/vlHnAMU4+8y6sfRQDGiCIWPSyypIWfEA6/O+SsEQ7vZ/b4mXlghUmxL+o2emsCI1e9PORvm5yc9Y/htN3Ju0x6ElHnih7MJT6/YUMISuyob9/mbw8Vf49M7H2t3AE5QIYcjqTwWJcwMlq5i9XfW2QLGH7K5i8\n-----END CERTIFICATE-----`;
+const { pubKey, secretKey } = process.env;
+const publicKey = `-----BEGIN CERTIFICATE-----\n${pubKey}\n-----END CERTIFICATE-----`;
 
 const authRouter = express.Router();
 export default authRouter;
 
-authRouter.post("/auth", (req, res, next) => {
-  // AigFvA2ubOR67BZFOwRi._ZyFY7Q9qD~8~
-  jwt.verify(req.headers.authorization, pubKey, { algorithms: ["RS256"] }, function (err, decoded) {
-    console.log("############################", err, decoded);
-    writeFbData("users", decoded);
-  });
-  res.status(200).send({
-    message: "Welcome to MS test API.",
-  });
+authRouter.post("/auth", async (req, res, next) => {
+  let msUserInDb;
+  const { type, email, password } = req.body;
+  const idToken = req.headers.authorization;
+  let token,
+    responseObject = {};
+  const genToken = async (data) => await jwt.sign(data, secretKey);
+  const responseHandler = (resObj) => res.status(resObj.status || 204).send(resObj);
+  try {
+    type === "local" &&
+      (await readFbData("users", (users) => {
+        Object.keys(users).map(async (key) => {
+          if (users[key].email === email || users[key].preferred_username === email) token = await genToken(email);
+        });
+        responseObject.message = "login successfully";
+        responseObject.status = 200;
+        responseObject.token = token;
+        responseHandler(responseObject);
+      }));
+    type === "microsoft" &&
+      (await jwt.verify(idToken, publicKey, { algorithms: ["RS256"] }, async (err, decoded) => {
+        await readFbData("users", async (users) => {
+          token = await genToken({ email: decoded.preferred_username });
+          Object.keys(users).map(async (key) => {
+            if (users[key].preferred_username === decoded.preferred_username) {
+              msUserInDb = true;
+            }
+          });
+          responseObject.message = "login successfully";
+          responseObject.status = 200;
+          responseObject.token = token;
+          responseHandler(responseObject);
+        });
+        if (!msUserInDb) writeFbData("users", decoded);
+      }));
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 /**
  * @swagger
