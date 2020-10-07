b6cf4b8747037d2154ea321c514035c422d52827
hezzie <hezronchelimo.hc@gmail.com>
2020-10-07 21:57:29 +0200

    edge case: - ensure that users saved have unique emails

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,38 +3,39 @@ import { writeFbData, readFbData } from "../utils/fb";
 
 const { pubKey, secretKey } = process.env;
 const publicKey = `-----BEGIN CERTIFICATE-----\n${pubKey}\n-----END CERTIFICATE-----`;
-
+let token,
+  responseObject = {};
+const genToken = async (data) => await jwt.sign(data, secretKey);
+const responseHandler = (resObj, res) => res.status(resObj.status || 204).send(resObj);
 const signInController = async (req, res, next) => {
   let msUserInDb;
   const { type, email, password } = req.body;
   const idToken = req.headers.authorization;
-  let token,
-    responseObject = {};
-  const genToken = async (data) => await jwt.sign(data, secretKey);
-  const responseHandler = (resObj) => res.status(resObj.status || 204).send(resObj);
+  const sendLoginResponse = () => {
+    responseObject.message = "login successfully";
+    responseObject.status = 200;
+    responseObject.token = token;
+    responseHandler(responseObject, res);
+  };
   try {
     type === "local" &&
       (await readFbData("users", (users) => {
         Object.keys(users).map(async (key) => {
           if (users[key].email === email || users[key].preferred_username === email) token = await genToken(email);
         });
-        responseObject.message = "login successfully";
-        responseObject.status = 200;
-        responseObject.token = token;
-        responseHandler(responseObject);
+        sendLoginResponse();
       }));
     type === "microsoft" &&
       (await jwt.verify(idToken, publicKey, { algorithms: ["RS256"] }, async (err, decoded) => {
         await readFbData("users", async (users) => {
           token = await genToken({ email: decoded.preferred_username });
           Object.keys(users).map(async (key) => {
-            if (users[key].preferred_username === decoded.preferred_username) msUserInDb = true;
+            if (users[key].email === decoded.preferred_username) msUserInDb = true;
           });
-          if (!msUserInDb) writeFbData("users", decoded);
-          responseObject.message = "login successfully";
-          responseObject.status = 200;
-          responseObject.token = token;
-          responseHandler(responseObject);
+          if (!msUserInDb) {
+            writeFbData("users", { email: decoded.preferred_username, password: "microsoft", name: decoded.name });
+          }
+          sendLoginResponse();
         });
       }));
   } catch (error) {
@@ -43,9 +44,21 @@ const signInController = async (req, res, next) => {
 };
 
 export const signupController = async (req, res, next) => {
-  console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXX", req.body);
+  let userInDb;
   const { name, email, password } = req.body;
-  writeFbData("users", { name, email, password });
+  await readFbData("users", async (users) => {
+    Object.keys(users).map(async (key) => {
+      if (users[key].email === email) userInDb = true;
+    });
+    if (!userInDb) {
+      writeFbData("users", { name, email, password });
+    }
+  });
+  token = await genToken({ email });
+  responseObject.message = "Signup successful";
+  responseObject.status = 200;
+  responseObject.token = token;
+  responseHandler(responseObject, res);
 };
 
 export { signInController };
