91b9c23fa920048c490b567b8eea9d5c9c5e1a08
hezzie <hezronchelimo.hc@gmail.com>
2020-10-11 08:24:26 +0200

    fetch public key either from cache or from ms url

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,9 +1,11 @@
 import { readFbData } from "../utils/fb";
 import { responseHandler } from "../utils";
 import jwt from "jsonwebtoken";
+import axios from "axios";
+import fs from "fs";
 
-const { pubKey, secretKey } = process.env;
-const publicKey = `-----BEGIN CERTIFICATE-----\n${pubKey}\n-----END CERTIFICATE-----`;
+const { pubKeyUrl } = process.env;
+// const publicKey = `-----BEGIN CERTIFICATE-----\n${pubKey}\n-----END CERTIFICATE-----`;
 
 const checkConflict = async (req, res, next) => {
   let userInDb;
@@ -17,16 +19,59 @@ const checkConflict = async (req, res, next) => {
     return responseHandler({ status: 400, message: "User with the given email already exist" }, res);
   });
 };
+const readPublicKey = (cb) => {
+  fs.readFile("pubKey.log", "utf8", function (err, data) {
+    if (err) {
+      console.log(err);
+      cb(false);
+    }
+    cb(data);
+  });
+};
+const newPublicKey = async () => {
+  try {
+    const rs = await axios(pubKeyUrl);
+    const pbKey = rs.data["keys"][0]["x5c"];
+    fs.writeFile("pubKey.log", pbKey[0], function (err) {
+      if (err) console.log(err);
+      console.log("Pubkey stored in log file");
+    });
+    return pbKey[0];
+  } catch (error) {
+    return false;
+  }
+};
 
 const verifyMsToken = async (req, res, next) => {
   const idToken = req.headers.authorization;
   const { type } = req.body;
   type === "microsoft" &&
-    (await jwt.verify(idToken, publicKey, { algorithms: ["RS256"] }, async (err, decoded) => {
-      if (err) return responseHandler({ status: 400, message: "Invalid token" }, res);
-      req.msUser = decoded;
-      next();
-    }));
+    readPublicKey(async (dta) => {
+      await jwt.verify(
+        idToken,
+        `-----BEGIN CERTIFICATE-----\n${dta}\n-----END CERTIFICATE-----`,
+        { algorithms: ["RS256"] },
+        async (err, decoded) => {
+          if (err) {
+            const tkn = await newPublicKey();
+            return await jwt.verify(
+              idToken,
+              `-----BEGIN CERTIFICATE-----\n${tkn}\n-----END CERTIFICATE-----`,
+              { algorithms: ["RS256"] },
+              async (err, decoded) => {
+                if (err) {
+                  return responseHandler({ status: 400, message: "Invalid token" }, res);
+                }
+                req.msUser = decoded;
+                return next();
+              }
+            );
+          }
+          req.msUser = decoded;
+          return next();
+        }
+      );
+    });
   type === "local" && next();
 };
 export { checkConflict, verifyMsToken };
